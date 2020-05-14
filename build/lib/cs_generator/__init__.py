755756acd6581a08b8c8d62a1f4bdd60dc8df10b
lihailin <redfort@qq.com>
2020-05-15 02:01:43 +0800

    add .gitignore

--- build/lib/cs_generator/__init__.py
+++
@@ -1,8 +0,0 @@
-from cs_generator.CSGenerator import CSGenerator, anno_to_types
-
-import cs_generator._constants as constants
-
-__author__ = constants.__author__
-__license__ = constants.__license__
-__url__ = constants.__url__
-__version__ = constants.__version__
--- build/lib/cs_generator/_constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Package metadata
-"""
-
-__author__ = "lihailin"
-__license__ = "MIT"
-__url__ = "https://github.com/fortfall/cs_generator"
-__version__ = "1.0.1"
